b5f17b722a9a4c65173c93837a7b443e1607ceb1
khuang9 <khuangfirst@gmail.com>
2026-03-12 02:01:25 -0400

    inference mode

--- scripts/huggingface_inference.py
+++ scripts/huggingface_inference.py
@@ -84,14 +84,15 @@ def main():
     inputs = inputs.to(model.device)
 
     # generate output
-    generated_ids = model.generate(**inputs, max_new_tokens=128)
-    generated_ids_trimmed = [
-        out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-    ]
-    output_text = processor.batch_decode(
-        generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-    )
-    print(output_text)
+    with torch.inference_mode():
+        generated_ids = model.generate(**inputs, max_new_tokens=128)
+        generated_ids_trimmed = [
+            out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+        ]
+        output_text = processor.batch_decode(
+            generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+        )
+        print(output_text)
 
 if __name__ == '__main__':
     main()
